401a3f056a19575abe01f7abaa0a44c3f1a963b1
Your Name <you@example.com>
2025-04-18 02:06:24 -0400

    new try

--- hashmap.c
+++ hashmap.c
@@ -107,7 +107,7 @@ Pair * searchMap(HashMap * map,  char * key) {
     long posicion = hash(key, map->capacity);
     
     while (map->buckets[posicion] != NULL && map->buckets[posicion]->key!= NULL){
-        if (map->buckets[posicion]->key == key){
+        if (is_equal(map->buckets[posicion]->key, key) == 0){
             Pair *buscado = (Pair*) calloc(1, sizeof(Pair));
             buscado->key = key;
             buscado->value = map->buckets[posicion]->value;
